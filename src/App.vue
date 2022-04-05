3dc71f7d1cd13ea01bc41325f9e472db0be2b779
caglar.durmus <caglardur@gmail.com>
2022-04-05 04:00:54 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <SignupForm />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue"
+import SignupForm from "./components/SignupForm.vue"
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+  name: "App",
+  components: { SignupForm }
+})
 </script>
 
 <style>
@@ -23,5 +20,9 @@ export default defineComponent({
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  margin: 0;
+  background: #eee;
 }
 </style>
